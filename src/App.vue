b5f72daf1d4c72e8300bce451c041c4026e72f06
mahmud9698 <starmahmud82@gmail.com>
2023-11-01 01:43:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Ninja Reaction Time</h1>
+  <button @click="start" v-bind:disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<!-- // challenge 
+// - when the game ends, show the results component
+// - output the score inside the result component -->
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Result.vue";
+export default {
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      //random time delay
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: lightgreen;
+  color: whitesmoke;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank" v-html="rank"></p>
+  </template>
+  
+  <script>
+  export default {
+      props: ['score'],
+      data() {
+          return {
+              rank : null
+          }
+      },
+      mounted() {
+          if (this.score < 250) {
+              this.rank = "You're Ninja!"
+          } else if (this.score < 400) {
+              this.rank = "Almost Ninja!<br><br> Good Rapid reflexes!"
+          } else {
+              this.rank = "Similar to snail hehe ..."
+          }   
+      }
+  }
+  </script>
+  
+  <style>
+      .rank {
+          font-size: 1.4em;
+          color: lightseagreen;
+          font-weight: bold;
+      }
+  </style>
